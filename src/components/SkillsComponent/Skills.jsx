37f73dd4e13f3bc237b239c9bf1126cd186bd4b9
dejv0ld <david_johnson_1992@hotmail.com>
2024-05-05 22:20:07 +0200

    made the horizontal scroll animation double and change direction on the new one.

--- src/components/SkillsComponent/Skills.jsx
+++ src/components/SkillsComponent/Skills.jsx
@@ -3,7 +3,7 @@ import './Skills.css';
 export const Skills = () => {
   return (
     <div className="skillsContainer">
-      <div className="scroll" style={{'--time': '15s'}}>
+      <div className="scroll" style={{ '--time': '20s' }}>
         <ul className="tagList scroller__inner">
           <li className="specItem">HTML</li>
           <li className="specItem">CSS</li>
@@ -11,8 +11,9 @@ export const Skills = () => {
           <li className="specItem">webdev</li>
           <li className="specItem">UX/UI</li>
           <li className="specItem">fullstack</li>
-          <li className="specItem">TS</li>
+          <li className="specItem">TypeScript</li>
           <li className="specItem">REACT</li>
+          <li className="specItem">Tailwind</li>
         </ul>
         <ul className="tagList scroller__inner">
           <li className="specItem">HTML</li>
@@ -21,8 +22,33 @@ export const Skills = () => {
           <li className="specItem">webdev</li>
           <li className="specItem">UX/UI</li>
           <li className="specItem">fullstack</li>
-          <li className="specItem">TS</li>
+          <li className="specItem">TypeScript</li>
           <li className="specItem">REACT</li>
+          <li className="specItem">Tailwind</li>
+        </ul>
+      </div>
+      <div className="scroll2" style={{ '--time': '20s' }}>
+        <ul className="tagList scroller__inner2">
+          <li className="specItem">SQL</li>
+          <li className="specItem">NoSQL</li>
+          <li className="specItem">Node</li>
+          <li className="specItem">Vue.js</li>
+          <li className="specItem">React Native</li>
+          <li className="specItem">Express.js</li>
+          <li className="specItem">APIs</li>
+          <li className="specItem">SPA</li>
+          <li className="specItem">Git</li>
+        </ul>
+        <ul className="tagList scroller__inner2">
+          <li className="specItem">SQL</li>
+          <li className="specItem">NoSQL</li>
+          <li className="specItem">Node</li>
+          <li className="specItem">Vue.js</li>
+          <li className="specItem">React Native</li>
+          <li className="specItem">Express.js</li>
+          <li className="specItem">APIs</li>
+          <li className="specItem">SPA</li>
+          <li className="specItem">Git</li>
         </ul>
       </div>
     </div>
